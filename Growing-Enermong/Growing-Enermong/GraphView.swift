55d159820e4ce238eed38d88b2b8046dafcce2c3
이씨안 <ffasang123@naver.com>
2017-04-08 04:11:25 +0900

    stash error

---
+++ Growing-Enermong/Growing-Enermong/GraphView.swift
@@ -0,0 +1,41 @@
+//
+//  GraphView.swift
+//  Growing-Enermong
+//
+//  Created by   minjae on 2017. 4. 8..
+//  Copyright © 2017년 minjae. All rights reserved.
+//
+
+import UIKit
+import ScrollableGraphView
+
+class GraphView: UIView {
+//    let graphView = ScrollableGraphView(frame: CGRect(x: 8, y: 40, width: 272, height: 160))
+//    
+//    graphView.backgroundFillColor = UIColor.colorFromHex("#333333")
+//    
+//    graphView.rangeMax = 50
+//    
+//    graphView.lineWidth = 1
+//    graphView.lineColor = UIColor.colorFromHex("#777777")
+//    graphView.lineStyle = ScrollableGraphViewLineStyle.Smooth
+//    
+//    graphView.shouldFill = true
+//    graphView.fillType = ScrollableGraphViewFillType.Gradient
+//    graphView.fillColor = UIColor.colorFromHex("#555555")
+//    graphView.fillGradientType = ScrollableGraphViewGradientType.Linear
+//    graphView.fillGradientStartColor = UIColor.colorFromHex("#555555")
+//    graphView.fillGradientEndColor = UIColor.colorFromHex("#444444")
+//    
+//    graphView.dataPointSpacing = 80
+//    graphView.dataPointSize = 2
+//    graphView.dataPointFillColor = UIColor.whiteColor()
+//    
+//    graphView.referenceLineLabelFont = UIFont.boldSystemFontOfSize(8)
+//    graphView.referenceLineColor = UIColor.whiteColor().colorWithAlphaComponent(0.2)
+//    graphView.referenceLineLabelColor = UIColor.whiteColor()
+//    graphView.dataPointLabelColor = UIColor.whiteColor().colorWithAlphaComponent(0.5)
+//    
+//    graphView.setData(data, withLabels: labels)
+//    self.view.addSubview(graphView)CG
+}
