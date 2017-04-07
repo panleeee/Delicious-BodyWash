fc962118691a2764259bc17555bd7ac5b240b971
minjaeJo <alswo2722@naver.com>
2017-04-08 08:27:28 +0900

    뭐야... 찌꺼기

--- MyPlayground.playground/Contents.swift
+++ MyPlayground.playground/Contents.swift
@@ -2,18 +2,32 @@
 
 import UIKit
 
-//var str = "Hello, playground"
-//
-//var shouldKeepRunning = true
-//var json = [String:String]()
-//URLSession(configuration: URLSessionConfiguration.default).dataTask(with: URL(string: "https://enermong.scalingo.io/api/1/getUsage")!) {
-//    (data, response, error) in
-//    var resultHTML = String(data: data!, encoding: String.Encoding.utf8)
-//    print(resultHTML!)
-//    //json =
-//    shouldKeepRunning = false
-//    }.resume()
-//while shouldKeepRunning && RunLoop.current.run(mode: .defaultRunLoopMode, before: .distantFuture) {
-//}
-//
-print("hello")
+let config = URLSessionConfiguration.default // Session Configuration
+let session = URLSession(configuration: config) // Load configuration into Session
+let url = URL(string: "https://enermong.scalingo.io/api/1/getUsage")!
+
+let task = session.dataTask(with: url, completionHandler: {
+    (data, response, error) in
+    if error != nil {
+        print(error!.localizedDescription)
+    } else {
+        do {
+            if let json = try JSONSerialization.jsonObject(with: data!, options: .allowFragments) as? [String: Any]{
+                //Implement your logic
+                if let names = json["result"] as? [String] {
+                    print(names)
+                }
+                print(json)
+                
+            }
+            
+        } catch {
+            
+            print("error in JSONSerialization")
+            
+        }
+        
+        
+    }
+})
+task.resume()
