8498f61471d2c97dce2a905866b7c7a560dde7f6
이씨안 <ffasang123@naver.com>
2017-04-07 21:15:10 +0900

    Edited to Statistics View Controller
    통계 뷰의 디자인을 디자인 시안과 일치시켰습니다. 또한, StatisticsViewController.swift파일을 추가하였습니다.

---
+++ Growing-Enermong/Growing-Enermong/StatisticsViewController.swift
@@ -0,0 +1,35 @@
+//
+//  StatisticsViewController.swift
+//  Growing-Enermong
+//
+//  Created by 이씨안 on 2017. 4. 7..
+//  Copyright © 2017년 minjae. All rights reserved.
+//
+
+import UIKit
+
+class StatisticsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.navigationController?.navigationBar.barTintColor = UIColor(colorLiteralRed: 255/255, green: 210/255, blue: 0, alpha: 1)
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
