cb0b1528fdc039401bb11b505b717c41f709d9a1
minjaeJo <alswo2722@naver.com>
2017-04-08 05:41:33 +0900

    refreshButton

--- MyPlayground.playground/Contents.swift
+++ MyPlayground.playground/Contents.swift
@@ -2,4 +2,18 @@
 
 import UIKit
 
-var str = "Hello, playground"
+//var str = "Hello, playground"
+//
+//var shouldKeepRunning = true
+//var json = [String:String]()
+//URLSession(configuration: URLSessionConfiguration.default).dataTask(with: URL(string: "https://enermong.scalingo.io/api/1/getUsage")!) {
+//    (data, response, error) in
+//    var resultHTML = String(data: data!, encoding: String.Encoding.utf8)
+//    print(resultHTML!)
+//    //json =
+//    shouldKeepRunning = false
+//    }.resume()
+//while shouldKeepRunning && RunLoop.current.run(mode: .defaultRunLoopMode, before: .distantFuture) {
+//}
+//
+print("hello")
